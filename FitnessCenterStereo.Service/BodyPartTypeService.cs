ddd862aaa3d804a68f7e979d921b6531e2822148
Matija Almaši <matija@matijaalmasi.com>
2019-08-22 16:38:26 +0200

    Add AppDbContext to Service and Repo, Temporary API changes

--- FitnessCenterStereo.Service/BodyPartTypeService.cs
+++ FitnessCenterStereo.Service/BodyPartTypeService.cs
@@ -6,39 +6,47 @@ using FitnessCenterStereo.Model.Common;
 using FitnessCenterStereo.Service.Common;
 using FitnessCenterStereo.Repository.Common;
 using FitnessCenterStereo.Repository;
-
+using FitnessCenterStereo.DAL.Data;
 
 namespace FitnessCenterStereo.Service
 {
     public class BodyPartTypeService : IBodyPartTypeService
     {
+
+        private ApplicationDbContext AppDbContext;
+
+        public BodyPartTypeService(ApplicationDbContext dbContext)
+        {
+            AppDbContext = dbContext;
+        }
+
         public IBodyPartType Create(IBodyPartType BodyPartType)
         {
-            IBodyPartTypeRepository BodyPart = new BodyPartTypeRepository();
+            IBodyPartTypeRepository BodyPart = new BodyPartTypeRepository(AppDbContext);
             return BodyPart.Create(BodyPartType);
         }
 
         public void Delete(Guid Id)
         {
-            IBodyPartTypeRepository BodyPart = new BodyPartTypeRepository();
+            IBodyPartTypeRepository BodyPart = new BodyPartTypeRepository(AppDbContext);
             BodyPart.Delete(Id);
         }
 
         public IEnumerable<IBodyPartType> Find(IFilter filter)
         {
-            IBodyPartTypeRepository BodyPart = new BodyPartTypeRepository();
+            IBodyPartTypeRepository BodyPart = new BodyPartTypeRepository(AppDbContext);
             return BodyPart.Find(filter);
         }
 
         public IBodyPartType Get(Guid Id)
         {
-            IBodyPartTypeRepository BodyPart = new BodyPartTypeRepository();
+            IBodyPartTypeRepository BodyPart = new BodyPartTypeRepository(AppDbContext);
             return BodyPart.Get(Id);
         }
 
         public void Update(Guid Id)
         {
-            IBodyPartTypeRepository BodyPart = new BodyPartTypeRepository();
+            IBodyPartTypeRepository BodyPart = new BodyPartTypeRepository(AppDbContext);
             BodyPart.Update(Id);
         }
     }
