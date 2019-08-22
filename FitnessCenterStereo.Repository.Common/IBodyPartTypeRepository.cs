a9f8c69ba746e052aa62f3da133da95baee33e6e
Marko Grubesa <grubesa.marko97@gmail.com>
2019-08-22 10:40:36 +0200

    Fix

--- FitnessCenterStereo.Repository.Common/IBodyPartTypeRepository.cs
+++ FitnessCenterStereo.Repository.Common/IBodyPartTypeRepository.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FitnessCenterStereo.Common;
 
 namespace FitnessCenterStereo.Model.Common
 {
     public interface IBodyPartTypeRepository
     {
+        
         Guid GetById(Guid Id);
         String GetByName(String Name);
         String GetByAbbreviation(String Abbrevation);
