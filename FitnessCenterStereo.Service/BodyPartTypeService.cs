3d169d4195209d5338f56c9a074a947fc8813ec4
Matija Almaši <matija@matijaalmasi.com>
2019-08-22 14:41:45 +0200

    Add blank BodyPartTypeService

---
+++ FitnessCenterStereo.Service/BodyPartTypeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FitnessCenterStereo.Common;
+using FitnessCenterStereo.Model.Common;
+
+namespace FitnessCenterStereo.Service
+{
+    class BodyPartTypeService
+    {
+
+    }
+}
