6407c418549c05f67a4f11cca01e42ea926e38bc
Marko Grubesa <grubesa.marko97@gmail.com>
2019-08-23 12:59:20 +0200

    FixforRepository

--- FitnessCenterStereo.Repository/ExerciseScheduleRepository.cs
+++ FitnessCenterStereo.Repository/ExerciseScheduleRepository.cs
@@ -1,4 +1,5 @@
 ﻿using FitnessCenterStereo.Common;
+using FitnessCenterStereo.Model.Common;
 using FitnessCenterStereo.Repository.Common;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,29 @@ namespace FitnessCenterStereo.Repository
 {
     class ExerciseScheduleRepository : IExerciseScheduleRepository
     {
-       edException();
+        public IExerciseSchedule Create(IExerciseSchedule exerciseSchedule)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IExerciseSchedule> Find(IFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IExerciseSchedule Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(IExerciseSchedule exerciseSchedule)
+        {
+            throw new NotImplementedException();
         }
     }
 }
