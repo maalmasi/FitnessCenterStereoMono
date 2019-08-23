a5cbad34c4947b9bb10ef0f6bcd252eedbe2d149
Matija Almaši <matija@matijaalmasi.com>
2019-08-23 11:41:17 +0200

    Add automapper configuration

---
+++ FitnessCenterStereo.WebApi/Mappings/Mappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FitnessCenterStereo.Model;
+using FitnessCenterStereo.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessCenterStereo.WebApi.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Add as many of these lines as you need to map your objects
+            CreateMap<BodyPartType, IBodyPartType>().ReverseMap();
+        }
+    }
+}
