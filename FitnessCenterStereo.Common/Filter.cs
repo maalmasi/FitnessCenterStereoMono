9c102743d8cf0d052096910aebdbf804665a1de8
Marko Grubesa <grubesa.marko97@gmail.com>
2019-08-26 14:04:37 +0200

    FilterClassFix

--- FitnessCenterStereo.Common/Filter.cs
+++ FitnessCenterStereo.Common/Filter.cs
@@ -7,11 +7,33 @@ namespace FitnessCenterStereo.Common
 {
     public class Filter : IFilter
     {
-  
+
+        private int page;
+        private int records;
+
         public string SearchQuery { get ; set; }
         public string SortBy { get; set; }
         public bool SortAscending { get; set; }
-        public int Page { get; set; }
-        public int RecordsPerPage { get; set; }
+        public int Page {
+            get {
+                return page;
+            }
+            set{
+                if (value < 1) page = 1;
+                else page = value;
+            }                         
+        }
+        public int RecordsPerPage {
+            get { return records; }
+            set {
+
+                if (String.IsNullOrEmpty(value.ToString())) records = 10;
+
+                else if (value < 10) records = 10;
+                else if (value > 100) records = 100;
+                else records = value;
+
+            }
+        }
     }
 }
