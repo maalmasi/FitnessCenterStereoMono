bdaef2673aef83866555ca4ce25a8c2b8073e4ff
Matija Almaši <matija@matijaalmasi.com>
2019-09-02 14:59:18 +0200

    Update footer

--- UI/fitnesscenterstereo/src/components/footer.js
+++ UI/fitnesscenterstereo/src/components/footer.js
@@ -4,8 +4,12 @@ class Footer extends React.Component
 {
     render() {
         return (
-            <footer class="footer">
-                link
+            <footer class="page-footer fixed-bottom">
+                <div>
+                    <div class="footer-copyright text-center py-3">
+                        © 2019 Copyright
+                    </div>
+                </div>
             </footer>
         );
     }
