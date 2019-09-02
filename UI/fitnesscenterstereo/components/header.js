1290d3112e55005ace7ba559ab3f564ef3bef5e2
Matija Almaši <matija@matijaalmasi.com>
2019-09-02 11:55:48 +0200

    Add components

---
+++ UI/fitnesscenterstereo/components/header.js
@@ -0,0 +1,15 @@
+class Header extends React.Component
+{
+    render() {
+        return (
+            <nav id="nav_bar">
+                <ul>
+                    <li><a class="active"  href="Home"><span class="glyphicon glyphicon-home"></span></a></li>
+                    <li><a href="News">Newds</a></li>
+                    <li><a href="Contact">Contact</a></li>
+                    <li><a href="About">About us</a></li>
+                </ul>
+            </nav>
+        );
+      }
+}
