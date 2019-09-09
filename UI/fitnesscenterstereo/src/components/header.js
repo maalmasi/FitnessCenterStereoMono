8f265d22d6f405648af021d530f4d842fd34834a
Matija Almaši <matija@matijaalmasi.com>
2019-09-09 16:43:05 +0200

    Add simple form to DietType, fix header classes

--- UI/fitnesscenterstereo/src/components/header.js
+++ UI/fitnesscenterstereo/src/components/header.js
@@ -6,8 +6,8 @@ class Header extends React.Component
         return (
             <nav id="nav_bar">
                 <ul>
-                    <li><a class="active" href="Home"><span class="glyphicon glyphicon-home"></span></a></li>
-                    <li><a href="News">News</a></li>
+                    <li><a className="active" href="Home"><span className="glyphicon glyphicon-home"></span></a></li>
+                    <li><a href="News">Newds</a></li>
                     <li><a href="Contact">Contact</a></li>
                     <li><a href="About">About us</a></li>
                 </ul>
