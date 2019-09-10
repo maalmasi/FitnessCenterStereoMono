50760cd32633347e1b172458f21e6519bcfa0a46
Matija Almaši <matija@matijaalmasi.com>
2019-09-10 09:57:29 +0200

    Fix DietTypeEditForm import

--- UI/fitnesscenterstereo/src/pages/crud/dietType/DietTypeEditForm.js
+++ UI/fitnesscenterstereo/src/pages/crud/dietType/DietTypeEditForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Form from './DietTypeForm';
+import DietTypeForm from './DietTypeForm';
 import form from './formFields';
 
 export default class DietTypeEditForm extends React.Component {
     render() {
         return(
-            <Form form={form} />
+            <DietTypeForm form={form} />
         );
     }
 }
