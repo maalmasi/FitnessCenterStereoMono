0e8f2d9a90c9f30722763e680c2c05f88395ced6
Matija Almaši <matija@matijaalmasi.com>
2019-09-10 11:26:46 +0200

    Fix DietTypeEditForm

--- UI/fitnesscenterstereo/src/pages/crud/dietType/DietTypeEditForm.js
+++ UI/fitnesscenterstereo/src/pages/crud/dietType/DietTypeEditForm.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import DietTypeForm from './DietTypeForm';
-import form from './formFields';
+import { observer } from 'mobx-react';
+import SimpleInput from './inputs/SimpleInput';
 
-export default class DietTypeEditForm extends React.Component {
-    render() {
-        return(
-            <DietTypeForm form={form} />
-        );
-    }
-}
+
+const $btn = 'btn-primary';
+export default observer(({ form }) => (
+    <form onSubmit={form.onSubmit}>
+        <SimpleInput field={form.$('name')} />
+        <SimpleInput field={form.$('ingridients')} />
+
+        <br />
+        <button type="submit" className={$btn} onClick={form.onSubmit}>Submit</button>
+        <button type="button" className={$btn} onClick={form.onClear}>Clear</button>
+        <button type="button" className={$btn} onClick={form.onReset}>Reset</button>
+
+        <p>{form.error}</p>
+    </form>
+));
