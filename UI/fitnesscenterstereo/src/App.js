49fe871275f574f62784b6fb4e95a4a6fcd27b69
Matija Almaši <matija@matijaalmasi.com>
2019-09-10 11:05:12 +0200

    Add component for FormToast, add import to App

--- UI/fitnesscenterstereo/src/App.js
+++ UI/fitnesscenterstereo/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import DietTypeEditForm from './pages/crud/dietType/DietTypeEditForm.js';
 
 
 function App() {
