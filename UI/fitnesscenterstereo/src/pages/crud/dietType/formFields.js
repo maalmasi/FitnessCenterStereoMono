7161b690f21d2b3ece2564af1326ad288593c9a7
Matija Almaši <matija@matijaalmasi.com>
2019-09-10 09:26:40 +0200

    Add react bootstrap, update DietTypeForm

---
+++ UI/fitnesscenterstereo/src/pages/crud/dietType/formFields.js
@@ -0,0 +1,32 @@
+import MobxReactForm from "mobx-react-form";
+import dvr from "mobx-react-form/lib/validators/DVR";
+import validatorjs from "validatorjs";
+
+const plugins = {
+  dvr: dvr(validatorjs),
+};
+
+const fields = [{
+    name: 'name',
+    label: 'Name',
+    placeholder: 'Enter diet name',
+    rules: 'required|string|between:3, 25',
+  },{
+    name: 'ingridients',
+    label: 'Ingridients',
+    placeholder: 'Enter ingredients',
+    rules: 'required|string|between:5,100',
+  }];
+
+  const hooks = {
+    onSuccess(form) {
+      alert('Form is valid! Send the request here.');
+      console.log('Form Values!', form.values());
+    },
+    onError(form) {
+      alert('Form has errors!');
+      console.log('All form errors', form.errors());
+    }
+  }
+
+  export default new MobxReactForm({ fields }, { plugins, hooks });
