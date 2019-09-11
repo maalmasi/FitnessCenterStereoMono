ed333fb889f5140e1eb4d55afad554b208f8d483
Matija Almaši <matija@matijaalmasi.com>
2019-09-11 15:28:58 +0200

    Fix routing

--- UI/fitnesscenterstereo/src/components/SideBar.js
+++ UI/fitnesscenterstereo/src/components/SideBar.js
@@ -1,30 +1,26 @@
 import SideNav, { MenuIcon } from 'react-simple-sidenav';
 import React from 'react';
-import { inject,observer } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 
 
 
- class SideBar extends React.Component {
+class SideBar extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = { showNav: false };
-       this.handleClick = this.handleClick.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick (name){
-
+    handleClick(e) {
         const { rootStore } = this.props;
-        rootStore.routerStore.goTo(name);
+        rootStore.routerStore.goTo(e);
     };
 
     render() {
-
         return (
-            
             <div>
                 <button onClick={() => this.setState({ showNav: true })}>Btn</button>
-
                 <SideNav
                     showNav={this.state.showNav}
                     onHideNav={() => this.setState({ showNav: false })}
@@ -33,22 +29,13 @@ import { inject,observer } from 'mobx-react';
                     backgroundColor='#FFFFFF'
                     items={[
                         <div>
-                          
-                            <a  onClick={this.handleClick('')}>Trainer</a>
-                            <a onClick={this.handleClick}>Home</a>
-
-
+                            <a onClick={() => this.handleClick("trainer")}>Trainer</a>
+                            <a onClick={() => this.handleClick("")}>Home</a>
                         </div>
                     ]} />
-
             </div>
-           
-
         );
-
-
     }
-
 }
 
 export default inject('rootStore')(observer(SideBar));
