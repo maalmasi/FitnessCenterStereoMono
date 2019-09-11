5f27ca5686762b43f994762cc866a797e2ce6f4c
Franjo <franjojosip.jukic2@gmail.com>
2019-09-11 15:44:20 +0200

    Add Layout SideBar

--- UI/fitnesscenterstereo/src/components/Layout.js
+++ UI/fitnesscenterstereo/src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import SideBar from './SideBar';
 
 export default (props) => {
     return (
         <React.Fragment>
             <Header />
+            <SideBar />
             {props.children}
             <Footer />
         </React.Fragment>
