5724f5542149e8023b81b654ab9d984bb8d07155
Matija Almaši <matija@matijaalmasi.com>
2019-09-12 12:05:48 +0200

    Add components to shell

--- UI/fitnesscenterstereo/src/shell.js
+++ UI/fitnesscenterstereo/src/shell.js
@@ -3,19 +3,23 @@ import { inject } from 'mobx-react';
 import { RouterView } from 'mobx-state-router';
 import Home from './components/Home';
 import NotFoundPage from './components/NotFoundPage';
+import BodyPartType from './pages/crud/bodyPartType/BodyPartType';
+import ComplexityLevelType from './pages/crud/complexityleveltype/ComplexityLevelType';
+import ComplexityLevelTypeEdit from './pages/crud/complexityleveltype/ComplexityLevelTypeEdit';
 import DietTypeEdit from './pages/crud/dietType/DietTypeEdit'
 import DietType from './pages/crud/dietType/DietType'
 import Trainer from './pages/crud/trainer/Trainer'
-import BodyPartType from './pages/crud/bodyPartType/BodyPartType';
 
 
 const viewMap = {
     home: <Home />,
     notFound: <NotFoundPage />,
-    dietType: <DietType />,
-    dietTypeEdit: <DietTypeEdit />,
-    trainer: <Trainer />,
-    body:<BodyPartType/>
+    body:<BodyPartType/>,
+    complexityleveltype: <ComplexityLevelType />,
+    complexityleveltypeedit: <ComplexityLevelTypeEdit />,
+    diettype: <DietType />,
+    diettypeedit: <DietTypeEdit />,
+    trainer: <Trainer />
 };
 
 export const Shell = inject('rootStore')(
