75b36c412ed6ce9ceaa27f4c18f9e68a23882c3e
Franjo <franjojosip.jukic2@gmail.com>
2019-09-12 09:52:58 +0200

    Rename Component

---
+++ UI/fitnesscenterstereo/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Navbar, Nav, Form, Button, FormControl } from 'react-bootstrap';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link1">About us</Nav.Link>
+            <Nav.Link href="#link2">Login/Register</Nav.Link>
+
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
