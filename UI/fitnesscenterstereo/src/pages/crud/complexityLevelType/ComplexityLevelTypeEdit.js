f871b1a49375fa84708239225d0dc39050c38b9a
Matija Almaši <matija@matijaalmasi.com>
2019-09-12 11:42:05 +0200

    Add ComplexityLevelType page

---
+++ UI/fitnesscenterstereo/src/pages/crud/complexityLevelType/ComplexityLevelTypeEdit.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { inject, observer } from 'mobx-react';
+import Layout from '../../../components/Layout'
+import ComplexityLevelTypeForm from './ComplexityLevelTypeForm';
+
+
+@observer
+class ComplexityLevelTypeEdit extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Layout>
+                    <ComplexityLevelTypeForm />
+                </Layout>
+            </React.Fragment>
+        );
+    }
+}
+
+export default ComplexityLevelTypeEdit;
+
+
