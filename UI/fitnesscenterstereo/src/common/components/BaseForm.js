928ff1b1907a7bb31e1aabc1c43c1df2c8d88883
Matija Almaši <matija@matijaalmasi.com>
2019-09-20 11:13:18 +0200

    Fix BaseForm

--- UI/fitnesscenterstereo/src/common/components/BaseForm.js
+++ UI/fitnesscenterstereo/src/common/components/BaseForm.js
@@ -5,28 +5,13 @@ import MobxReactForm from "mobx-react-form";
 import SimpleInput from '../SimpleInput';
 
 export default class BaseForm extends MobxReactForm {
-    constructor(props) {
+    constructor({ hooks, ...otherProps }) {
         const options = {
             validateOnChange: true
         };
         const plugins = {
             dvr: dvr(validatorjs),
         };
-        super({props, options, plugins});
-    }
-    render() {
-        return (
-            <form onSubmit={this.form.onSubmit}>
-                {this.props.children}
-                {this.props.fields.forEach(element => {
-                    return <SimpleInput field={element}/>
-                })}
-                <br />
-                <button type="submit" disabled={!this.form.isValid} className={'btn-primary'}>Submit</button>
-                <button type="button" className={'btn-secondary'} onClick={this.form.onClear}>Clear</button>
-                <button type="button" className={'btn-secondary'} onClick={this.form.onReset}>Reset</button>
-                <p>{this.form.error}</p>
-            </form>
-        )
+        super(otherProps, { hooks, options, plugins });
     }
 }
