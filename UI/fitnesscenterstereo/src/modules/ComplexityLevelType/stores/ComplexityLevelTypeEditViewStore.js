b6912b581e80265b9e83bbdd2d586469827b4518
Matija Almaši <matija@matijaalmasi.com>
2019-09-20 12:17:38 +0200

    Add update and create functionality

--- UI/fitnesscenterstereo/src/modules/ComplexityLevelType/stores/ComplexityLevelTypeEditViewStore.js
+++ UI/fitnesscenterstereo/src/modules/ComplexityLevelType/stores/ComplexityLevelTypeEditViewStore.js
@@ -5,24 +5,16 @@ import ComplexityLevelTypeForm from '../components/ComplexityLevelTypeForm';
 class ComplexityLevelTypeEditViewStore {
     constructor(rootStore) {
         this.dataStore = rootStore.complexityLevelTypeModuleStore.complexityLevelTypeDataStore;
-        // if (window.location.pathname.lastIndexOf('/') !== 0) {
-        //     this.edit = true;
-        //     this.lastSlashInUrl = window.location.pathname.lastIndexOf('/');
-        //     this.itemToUpdateId = window.location.pathname.substr(this.lastSlashInUrl + 1);
-        //     this.get();
-        // } else {
-        //     this.edit = false;
-        //     this.isLoading = false;
-        // }
         const id = rootStore.routerStore.routerState.params.id;
         if (id != null) {
-            this.dataStore.get(id).then((model) => {
-                //runInAction(() => {
+            this.dataStore.get(id).then(({ name, abbreviation, id }) => {
+                runInAction(() => {
                     this.form = new ComplexityLevelTypeForm({
-                        values: model,
+                        values: { name, abbreviation },
                         hooks: {
                             onSuccess: (form) => {
                                 console.log("onSuccess: ", form.values());
+                                this.onUpdate(id);
                             },
                             onError: (form) => {
                                 console.log("onError: ", form.values());
@@ -30,7 +22,7 @@ class ComplexityLevelTypeEditViewStore {
                         }
                     });
                     this.isLoading = false;
-                //});
+                });
             }
             );
         } else {
@@ -38,6 +30,7 @@ class ComplexityLevelTypeEditViewStore {
                 hooks: {
                     onSuccess: (form) => {
                         console.log("onSuccess: ", form.values());
+                        this.onCreate();
                     },
                     onError: (form) => {
                         console.log("onError: ", form.values());
@@ -53,39 +46,9 @@ class ComplexityLevelTypeEditViewStore {
     @observable isLoading = true;
     @observable errorMessage = "";
 
-    initializeForm() {
-        const values = {
-            "name": this.item.name,
-            "abbreviation": this.item.abbreviation
-        };
-        const hooks = {
-            onSuccess(form) {
-                console.log('Form Values!', form.values());
-                toaster.notify('Form is valid!', {
-                    duration: 2000
-                })
-                this.onUpdate();
-            },
-            onError(form) {
-                console.log('All form errors', form.errors());
-                toaster.notify('Form is invalid!', {
-                    duration: 2000
-                })
-            }
-        }
-
-        this.form = new ComplexityLevelTypeForm({ values, hooks });
-        this.isLoading = false;
-    }
-
-    @action.bound async get() {
-        // this.isLoading = true;
-        // this.item = await (this.dataStore.get(this.itemToUpdateId));
-    }
-
-    @action.bound async onUpdate() {
+    async onUpdate(id) {
         try {
-            this.response = await (this.dataStore.update(this.form.values(), this.itemToUpdateId));
+            this.response = await (this.dataStore.update(this.form.values(), id));
             this.response ?
                 toaster.notify('Update successful!', { duration: 2000 })
                 :
@@ -97,10 +60,11 @@ class ComplexityLevelTypeEditViewStore {
 
     @action.bound async onCreate() {
         try {
-            this.item.name = this.itemName;
-            this.item.abbreviation = this.itemAbrv;
-            this.response = await (this.dataStore.create(this.item));
-            console.log(this.response);
+            this.response = await (this.dataStore.create(this.form.values()));
+            this.response ?
+                toaster.notify('Create successful!', { duration: 2000 })
+                :
+                toaster.notify('Create failed!', { duration: 2000 })
         } catch (error) {
             console.log(error);
         }
